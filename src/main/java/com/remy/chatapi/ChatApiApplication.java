9a4535c17f69d0cc0f955cb0509faaf8803be2e7
yongho <yongho612@gmail.com>
2024-08-20 18:40:32 +0900

    Moved API keys to .env file

--- src/main/java/com/remy/chatapi/ChatApiApplication.java
+++ src/main/java/com/remy/chatapi/ChatApiApplication.java
@@ -2,11 +2,19 @@ package com.remy.chatapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ChatApiApplication {
 
 	public static void main(String[] args) {
+		//Load .env file
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Set system properties for Spring to pick up
+		System.setProperty("OPENAI_API_KEY", dotenv.get("OPENAI_API_KEY"));
+		System.setProperty("SOLAR_API_KEY", dotenv.get("SOLAR_API_KEY"));
+
 		SpringApplication.run(ChatApiApplication.class, args);
 	}
 
